a1d656a7ac07878dc1969247e9562db93007529f
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-15 20:22:59 -0600

    moved project to an organization with repo for the team

---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Order = require('./Order');
+const Product = require('./Product');
+const Category = require('./Category');
+module.exports = { User, Order, Product, Category };
